b58821157affc64ba0478b8bb46d4795cf17b2cf
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2024-09-14 02:13:02 -0400

    Initial Commit

---
+++ memorybricksv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  memorybricksv2
+//
+//  Created by Kenneth Ye on 2024-09-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ memorybricksv2/memorybricksv2App.swift
@@ -0,0 +1,17 @@
+//
+//  memorybricksv2App.swift
+//  memorybricksv2
+//
+//  Created by Kenneth Ye on 2024-09-14.
+//
+
+import SwiftUI
+
+@main
+struct memorybricksv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
