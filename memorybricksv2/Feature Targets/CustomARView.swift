c77450be47f0f548a760e0f0d81487a88c77f755
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2024-09-14 02:29:41 -0400

    core ar app setup

---
+++ memorybricksv2/Feature Targets/CustomARView.swift
@@ -0,0 +1,23 @@
+//
+//  CustomARView.swift
+//  memorybricksv2
+//
+//  Created by Kenneth Ye on 2024-09-14.
+//
+import ARKit
+import RealityKit
+import SwiftUI
+
+class CustomARView: ARView {
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+    }
+    
+    dynamic required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init() {
+        self.init(frame: UIScreen.main.bounds)
+    }
+}
---
+++ memorybricksv2/Feature Targets/CustomARViewRepresentable.swift
@@ -0,0 +1,18 @@
+//
+//  CustomARViewRepresentable.swift
+//  memorybricksv2
+//
+//  Created by Kenneth Ye on 2024-09-14.
+//
+
+import SwiftUI
+
+struct CustomARViewRepresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> CustomARView {
+        return CustomARView()
+    }
+    
+    func updateUIView(_ uiView: CustomARView, context: Context) {
+        
+    }
+}
